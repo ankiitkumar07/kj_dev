b75b3e00ea9bc05d6e60be4a0ba42c3e419a4d72
Ankit Kumar <ankiitkumar07@gmail.com>
2017-09-09 17:53:13 +0530

    photo gallery completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require ckeditor/init
 //= require bootstrap-sprockets
 //= require jquery
-//= require masonry.pkgd
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require ckeditor/init
 //= require_tree .
 
 
