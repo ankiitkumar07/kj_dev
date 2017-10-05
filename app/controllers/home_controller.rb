82078fefb67393441a6504a9f75ef5edfae3de15
Ankit Kumar <ankiitkumar07@gmail.com>
2017-10-05 12:00:12 +0530

    try

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	@page_title = 'Welcome Aboard'
   	@tags = ActsAsTaggableOn::Tagging.limit(8).includes(:tag).where(context: 'tags').map { |tagging| tagging.tag.name  }.uniq
-  	@top_five_posts = Post.order("created_at DESC").limit(6)
+  	@top_five_posts = Post.order("created_at DESC").limit(3)
   	@tag = ActsAsTaggableOn::Tag.find(1) 
   end
 end
