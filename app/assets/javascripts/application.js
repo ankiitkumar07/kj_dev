a8f73163bcdf55ea40b30d7c6429be9da8e89da5
Ankit Kumar <ankiitkumar07@gmail.com>
2018-02-08 00:38:13 +0530

    new landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require instafeed
 //= require jquery3
 //= require popper
 //= require bootstrap
